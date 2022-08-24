120b1595616ade68b1b0b73e69cbe65fa925e08b
Leandro de Oliveira Silva <leandrooliveirask8@hotmail.com>
2022-08-24 16:29:56 -0300

    Adding database access

--- workshop-javafx-jdbc/model/entities/Department.java
+++ workshop-javafx-jdbc/model/entities/Department.java
@@ -1,4 +1,4 @@
-package application.workshopjavafxjdbc.model.entities;
+package model.entities;
 
 import java.io.Serializable;
 
--- workshop-javafx-jdbc/src/main/java/module-info.java
+++ workshop-javafx-jdbc/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module application.workshopjavafxjdbc {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens application.workshopjavafxjdbc to MainView.fxml;
