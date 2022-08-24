2a83679cee1947a74adb796333e652c94b381a04
Leandro de Oliveira Silva <leandrooliveirask8@hotmail.com>
2022-08-24 15:20:10 -0300

    DepartmentList controller

--- workshop-javafx-jdbc/src/main/java/module-info.java
+++ workshop-javafx-jdbc/src/main/java/module-info.java
@@ -6,5 +6,7 @@ module application.workshopjavafxjdbc {
     opens application.workshopjavafxjdbc to MainView.fxml;
     opens application.workshopjavafxjdbc.gui;
     opens application.workshopjavafxjdbc.gui.util;
+    opens application.workshopjavafxjdbc.model.entities;
+
     exports application.workshopjavafxjdbc;
 }
